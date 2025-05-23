f95f7a38a0ce06a9cff922e6b65753de4cab2664
SounDoer <xichenphoto@gmail.com>
2025-05-23 16:36:18 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/GAD_Logo.ico',
 
   // Set the production url of your site here
-  url: 'https://gad.soundoer.com',
+  url: 'http://gad.soundoer.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
