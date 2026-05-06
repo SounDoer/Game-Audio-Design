fa7d0512ce3457eb0e0ed708be26aa614b18578a
SounDoer <SounDoer@users.noreply.github.com>
2026-05-06 10:04:08 +0000

    deploy: c99a274a5971cf5afad37f9e81df03ffd33b6e1e

---
+++ slidev/assets/md-C_x_k6x9.js
@@ -0,0 +1,6 @@
+import{E as e,Z as t,_ as n,_t as r,ht as i,v as a,y as o,z as s}from"./modules/shiki-CKKjaqEk.js";import{X as c,Z as l}from"./index-Bez0DfRh.js";import{t as u}from"./header-body-CEvMHfS8.js";var d={__name:`audio-spatialization.md__slidev_65`,setup(d){let{$slidev:f,$nav:p,$clicksContext:m,$clicks:h,$page:g,$renderContext:_,$frontmatter:v}=l();return m.setup(),(l,d)=>(s(),a(u,r(e(i(c)(i(v),64))),{body:t(e=>[d[0]||=n(`p`,null,[n(`img`,{src:`/slidev/what-is-wwise-spatial-audio.png`,alt:``})],-1),o(` <iframe 
+  src="https://www.audiokinetic.com/en/wwise/wwise-spatial-audio/" 
+  width="100%" 
+  height="420px" 
+  style="border: none;">
+</iframe> `)]),default:t(()=>[d[1]||=n(`h3`,null,`现成的解决方案`,-1),d[2]||=n(`p`,null,[n(`a`,{href:`https://www.audiokinetic.com/en/wwise/wwise-spatial-audio/`,target:`_blank`},`Wwise Spatial Audio`)],-1)]),_:1},16))}};export{d as default};
